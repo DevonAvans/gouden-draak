29906aa89586e8bf35feb6d1043dcb51b8fff70f
DevonAvans <d.thomassen1@student.avans.nl>
2022-06-09 23:52:36 +0200

    Added fonts

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,5 @@ mix.js("resources/js/app.js", "public/js")
     .vue()
     .sass("resources/sass/app.scss", "public/css")
     .sass("resources/sass/main.scss", "public/css");
+
+mix.copyDirectory('resources/fonts', 'public/fonts');
