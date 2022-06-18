6795b6a008db6b963a3089b3ec3df1a6eaeb83fb
DevonAvans <d.thomassen1@student.avans.nl>
2022-06-18 12:18:23 +0200

    breeze and login

---
+++ resources/views/components/containers/header-container.blade.php
@@ -0,0 +1,5 @@
+<section class="header-container">
+  @include('components.containers.image-container')
+  <p>De gouden draak</p>
+  @include('components.containers.image-mirror-container')
+</section>
