1f199b7701d9ca13d480335b61f970f36f1d978d
Koen van Tilburg <71671751+Xoik@users.noreply.github.com>
2022-06-18 15:50:52 +0200

    kanker

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Dish;
 
 class MenuController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('menu');
+        if($request->filled('search')){
+            $dishes = Dish::search($request->search)->get();
+        }else{
+            $dishes = Dish::all();
+        }
+        return view('menu', compact("dishes"));
     }
 }
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,5 +1,38 @@
 @extends('layouts.main')
 
 @section('content')
-<h2>hier komt menu</h2>
+<div class="container">
+    <form method="GET">
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            name="search"
+            value="{{ request()->get('search') }}"
+            class="form-control"
+            placeholder="Search..."
+            aria-label="Search"
+            aria-describedby="button-addon2">
+          <button class="btn btn-success" type="submit" id="button-addon2">Search</button>
+        </div>
+    </form>
+
+    <table class="table table-bordered data-table">
+        <thead>
+            <tr>
+                <th>Nummer</th>
+                <th>Naam</th>
+                <th>Categorie</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($dishes as $dish)
+            <tr>
+                <td>{{ $dish->menu_text }}</td>
+                <td>{{ $dish->name }}</td>
+                <td>{{ $dish->category->name }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
