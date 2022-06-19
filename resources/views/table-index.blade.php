6a5add4fdb077706c7a06f9df6a8aef9c598a6e2
Koen van Tilburg <71671751+Xoik@users.noreply.github.com>
2022-06-19 14:29:28 +0200

    tafellayout

--- resources/views/table-index.blade.php
+++ resources/views/table-index.blade.php
@@ -1,28 +1,20 @@
 @extends('layouts.main')
 
 @section('content')
+<link href="{{ asset('css/tables/tables.css') }}" rel="stylesheet">
 <a href="{{route('table.clear')}}">Verwijder alle medewerkers</a>
-<table class="table table-bordered data-table">
-    <thead>
-        <tr>
-            <th>Nummer</th>
-            <th>Seats</th>
-            <th>User</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($tables as $table)
-        <tr>
-            <td>{{ $table->id }}</td>
-            <td>{{ $table->seats }}</td>
-            <td>@if (isset($table->user_id))
+<div class="tablegrid">
+    @foreach($tables as $table)
+        <div class="table">
+            Tafelnummer: {{ $table->id }}<br>
+            Plekken: {{ $table->seats }}<br>
+            Bijbehorende medewerker: @if (isset($table->user_id))
                 {{ $table->user->name }}
             @else
-                Geen medewerker
-            @endif</td>
-            <td><a href="{{route('table.edit', $table->id)}}">Edit</a></th>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+                Niemand
+            @endif<br>
+            <a href="{{route('table.edit', $table->id)}}">Edit</a>
+        </div>
+    @endforeach
+</div>
 @endsection
